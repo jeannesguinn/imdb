a0ba8cbb1b40c25c037f67eafd3eceef845bed6f
Jeanne Guinn <jeannesguinn@gmail.com>
2016-05-18 11:11:51 -0500

    add welcome index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   devise_for :users
   resource :profile, except: [:destroy]
   # The priority is based upon order of creation: first created -> highest priority.
