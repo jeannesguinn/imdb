0fae16a6a1406fdd8302b33798026ddf0bf998af
Jeanne Guinn <jeannesguinn@gmail.com>
2016-05-18 16:23:23 -0500

    add actor views and routes

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,2 +1,49 @@
 class ActorsController < ApplicationController
+before_action :set_actor, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @actors = Actor.all
+  end
+
+  def show
+  end
+
+  def new
+    @actor = Actor.new
+  end
+
+  def create
+    @actor = Actor.new(actor_params)
+   if @actor.save
+     redirect_to @actor
+   else
+     render :new
+   end
+  end
+
+  def edit
+  end
+
+  def update
+    if @actor.update_attributes(actor_params)
+     redirect_to @actor
+   else
+     render :edit
+   end
+  end
+
+  def destroy
+    @actor.destroy
+    redirect_to actor_path
+  end
+
+
+  def set_actor
+      @actor = Actor.find(params[:id])
+  end
+
+  def actor_params
+      params.require(:actor).permit(:name, :year_of_birth, :thumbnail)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resource :profile, except: [:destroy]
-  resources :movies
+  resources :movies, :actors
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
